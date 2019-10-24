c86e73adc7c0efc0d2ecc551da5dc3d48a7f055d
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-10-24 18:34:31 -0500

    make sure that testing runs on Travis

--- tests/testthat/setup_auth.R
+++ tests/testthat/setup_auth.R
@@ -2,4 +2,5 @@ if (gargle:::secret_can_decrypt("boxr")) {
   raw <- gargle:::secret_read("boxr", "boxr-testing.json")
   text <- rawToChar(raw)
   box_auth_service(token_text = text)
+  message(has_jwt_token())
 }
