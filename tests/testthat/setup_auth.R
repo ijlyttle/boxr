ece17ad9c0d5fd4c4e2cbb1fb1598527bb6ae06d
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-10-24 19:01:06 -0500

    Update JWT token for testing

--- tests/testthat/setup_auth.R
+++ tests/testthat/setup_auth.R
@@ -2,5 +2,4 @@ if (gargle:::secret_can_decrypt("boxr")) {
   raw <- gargle:::secret_read("boxr", "boxr-testing.json")
   text <- rawToChar(raw)
   box_auth_service(token_text = text)
-  message(has_jwt_token())
 }
